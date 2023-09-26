3e5e415b5e241ea74c8c3e752b7fc4661e8391a1
katrinmelody <77362916+katrinmelody@users.noreply.github.com>
2023-09-26 22:07:13 +0300

    Remove entry code

--- src/index.js
+++ src/index.js
@@ -1,8 +1,2 @@
-const chalk = require('chalk')
 const config = require('./config')
 
-console.log(chalk.yellow('Your github token is:'))
-console.info(chalk.yellow(config.GITHUB_PERSONAL_ACCESS_TOKEN))
-
-// remove this line
-require('./example')
