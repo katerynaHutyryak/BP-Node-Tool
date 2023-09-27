29bea0f9a0ce17290a4c37b3d7e94aa216a61d3a
katrinmelody <77362916+katrinmelody@users.noreply.github.com>
2023-09-27 12:45:45 +0300

    Fetch commit comments

--- src/index.js
+++ src/index.js
@@ -1,2 +1,30 @@
+const axios = require('axios')
+
 const config = require('./config')
 
+const [owner, repo] = process.argv[3].split('/')
+const apiBase = 'https://api.github.com'
+
+const commentsArray = []
+
+async function fetchCommitComments () {
+    let response
+    let page = 1
+    do {
+        try{
+            response = await axios.get(`${apiBase}/repos/${owner}/${repo}/comments?per_page=100&page=${page}`, {
+                headers: {
+                    Authorization: `token ${config.GITHUB_PERSONAL_ACCESS_TOKEN}`,
+                    'X-GitHub-Api-Version': '2022-11-28' 
+                }
+            })
+        } catch (err) {
+            console.log(err)
+        }
+        commentsArray.push(...response.data)    
+        page++
+    } while (response.data.length === 100)
+    console.log(commentsArray.length)
+}
+
+fetchCommitComments()
